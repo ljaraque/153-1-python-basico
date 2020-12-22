8f347e4810ff8471d6ec63e8ba3817130229f3d4
ljaraque <ljaraque@yahoo.com>
2020-12-22 20:22:33 -0300

    actualización con getter y setters y capucitas rebeldes independientes de reino

--- clases/20201218_m3-clase2.py
+++ clases/20201218_m3-clase2.py
@@ -1,4 +1,3 @@
-
 class Yagan:
     num_ruedas = 4
     num_velocidades = 4
@@ -96,6 +95,7 @@ for index, caperucita in enumerate(caperucitas):
     print(index, " El color de la capa de la caperucita "+str(id(caperucita))+" es: " + caperucita.color_capa +" "+ caperucita.__class__.color_capa)
 
 
+# Agregar Reino y Caperucitas de tal forma que Caperucitas dejen de existir si reino deja de existir
 class Reino:
 
     def __init__(self, num_caperucitas):
@@ -113,6 +113,49 @@ class Reino:
         return lista_caperucitas
 
 reino_1 = Reino(9)
+for index, caperucita in enumerate(reino_1.caperucitas):
+    print(index, " El color de la capa de la caperucita "+str(id(caperucita))+" es: " + caperucita.color_capa +" "+ caperucita.__class__.color_capa)
+
+del reino_1
+try:
+    print(reino_1.caperucitas)
+except Exception as e:
+    print("Error, no puedo acceder a las caperucitas pues el reino_1 dejó de existir\n",e)
+
+
+# Agregar Reino y Caperucitas de tal forma que Caperucitas sigan existiendo si reino deja de existir
+class ReinoX:
+
+    def __init__(self, *caperucitas):
+        self.superficie = 100000
+        self.caperucitas = list(caperucitas)
+        print("Reino creado de "+str(self.superficie)
+                +" metros cuadrados y con caperucitas:\n"+str(self.caperucitas))
+        for caperucita in self.caperucitas:
+            print("Caperucita "+str(id(caperucita)))
+    
+caperucita_1 = Caperucita()
+caperucita_2 = Caperucita()
+caperucita_3 = Caperucita()
+caperucita_4 = Caperucita()
+caperucita_5 = Caperucita()
+
+reino_2 = ReinoX(caperucita_1, caperucita_2, caperucita_3, caperucita_4, caperucita_5)
+
+print("Destruyendo reino_2")
+del reino_2
+try:
+    print("Reino", id(reino_2))
+except Exception as e:
+    print(e)
+print("Caperucita", id(caperucita_1))
+print("Caperucita", id(caperucita_2))
+print("Caperucita", id(caperucita_3))
+print("Caperucita", id(caperucita_4))
+print("Caperucita", id(caperucita_5))
+
+print("El reino_2 ya no existe pero las caperucitas siguen existiendo")
+
 
 
 # sobrecarga de métodos
@@ -150,11 +193,13 @@ scraper_1.elemento_a_scrapear(etiquetas = ['precio', 'descripcion'],
 
 class Auto:
 
-    def __init__(self, posicion, nivel_estanque, color):
+    def __init__(self, marca, posicion, nivel_estanque, color):
         self.posicion = posicion
         self.__nivel_estanque = nivel_estanque
         self.color = color
         self.orientacion = 0
+        self.marca = marca
+        self.documento = {'marca': self.marca, 'color': self.color}
     
     def girar(self, grados):
         self.orientacion += grados
@@ -178,9 +223,10 @@ class Auto:
         print("Nivel Estanque: ", self.__nivel_estanque)
         print("Color: ", self.color)
         print("Orientación: ", self.orientacion)
+        print("Documentación:", self.documento)
 
 
-auto_mio = Auto(0, 50, "Rojo")
+auto_mio = Auto("Toyota", 0, 50, "Rojo")
 
 auto_mio.estado()
 auto_mio.cargar_combustible(30)
@@ -190,16 +236,151 @@ auto_mio._Auto__nivel_estanque = 10
 auto_mio.estado()
 print( dir(auto_mio))
 
-# estao no funciona pues es un metodo "privado"
-#auto_mio.reducir_nivel_estanque(10)
+# esto no funciona pues es un metodo "privado"
+try:
+    auto_mio.reducir_nivel_estanque(10)
+except Exception as e:
+    print("ERROR: Se ha intentado utilizar un método privado.!!!!\n", e)
+
 
 # yo podría usar el método si violo el espiritu de python
-#auto_mio._Auto__reducir_nivel_estanque(10)
+# ESTO NO SE HACE!!!!!!
+print("Antes del cambio")
+auto_mio.estado()
+auto_mio._Auto__reducir_nivel_estanque(10)
+print("Después del cambio")
+auto_mio.estado()
 
 # getter y setters
-'''
-auto_mio.posicion = 20
-auto_mio.posicion
+# Haremos que podamos modificar el color de nuestro auto
 
-auto_mio.set_posicion(20)
-'''
+auto_mio.color="Azul"
+print(auto_mio.color)
+auto_mio.estado()
+#   Enhorabuena!!!!! hemos podido moficar el colo porque es atributo no "privado"
+# Pero hemos destruido la consistencia de nuestros datos
+
+# Hemos cambiado el color, pero eso nos lleva a tener que:
+# Actualizar documentos de auto (CUEK!!!!!!) (EFE!!!!)
+# Tendremos que ir a hacer el trámite.
+# Se puso latero
+auto_mio.documento['color']="Azul"
+auto_mio.estado()
+
+# Enhorabuena Tío!!! Se ha recuperado la consistencia. Pero ahora es un cacho modificar la
+# variable pública color
+
+
+# Definimos una clase AutoX similar a Auto
+class AutoX:
+
+    def __init__(self, marca, posicion, nivel_estanque, color):
+        self.posicion = posicion
+        self.__nivel_estanque = nivel_estanque
+        self.__color = color
+        self.orientacion = 0
+        self.marca = marca
+        self.__documento = {'marca': self.marca, 'color': self.__color}
+    
+    def girar(self, grados):
+        self.orientacion += grados
+        return self.orientacion
+    
+    def avanzar(self, cantidad):
+        self.posicion += cantidad
+        self.__reducir_nivel_estanque(-0.1*cantidad)
+        return self.posicion
+    
+    def cargar_combustible(self, cantidad):
+        self.__nivel_estanque += cantidad
+        return self.__nivel_estanque
+    
+    def __reducir_nivel_estanque(self, cantidad):
+        self.__nivel_estanque += -cantidad
+
+    def set_color(self, color_nuevo):
+        self.__color = color_nuevo
+        self.__documento['color']=color_nuevo
+
+    def estado(self):
+        print("ESTADO")
+        print("Posición: ", self.posicion)
+        print("Nivel Estanque: ", self.__nivel_estanque)
+        print("Color: ", self.__color)
+        print("Orientación: ", self.orientacion)
+        print("Documentación:", self.__documento)
+
+print("\n\n\nCaso basado en AutoX:")
+auto_tuyo = AutoX("Toyota", 0, 50, "Rojo")
+auto_tuyo.estado()
+auto_tuyo.set_color("Azul")
+auto_tuyo.estado()
+
+
+# Finalmente luego de muchas reuniones de Pythonistas preocupados
+# de si su lenguaje tenía futuro.
+# Todos los códigos históricos se habían quebrado porque habría que utilizar
+# setters y getters como métdos de aquí en adelante y actualizar todos los
+# códigos de estilo python
+# Citaron a todos los medios para mostrar que habían salido de la depresión
+# generada por los otros programadores 
+# llegaron con la siguiente solución
+
+# Queremos insistir en usar estilo python para setear variables, y de todas formas
+# lograr que se actualice por ejemplo la documentación del auto
+
+class AutoY:
+
+    def __init__(self, marca, posicion, nivel_estanque, color):
+        self.posicion = posicion
+        self.__nivel_estanque = nivel_estanque
+        self.__color = color
+        self.orientacion = 0
+        self.marca = marca
+        self.__documento = {'marca': self.marca, 'color': self.__color}
+
+    ''' tip de investigación. Esto es un decorador en Python
+    def property(alguna_funcion):
+        #modifico la funcion 
+        return funcion_modificada
+    '''
+
+    @property
+    def color(self):
+        return self.__color
+
+    @color.setter
+    def color(self, color_nuevo):
+        self.__color = color_nuevo
+        self.__documento['color']=color_nuevo
+    
+    def girar(self, grados):
+        self.orientacion += grados
+        return self.orientacion
+    
+    def avanzar(self, cantidad):
+        self.posicion += cantidad
+        self.__reducir_nivel_estanque(-0.1*cantidad)
+        return self.posicion
+    
+    def cargar_combustible(self, cantidad):
+        self.__nivel_estanque += cantidad
+        return self.__nivel_estanque
+    
+    def __reducir_nivel_estanque(self, cantidad):
+        self.__nivel_estanque += -cantidad
+
+    def estado(self):
+        print("ESTADO")
+        print("Posición: ", self.posicion)
+        print("Nivel Estanque: ", self.__nivel_estanque)
+        print("Color: ", self.__color)
+        print("Orientación: ", self.orientacion)
+        print("Documentación:", self.__documento)
+
+print("\n\n\nCaso basado en AutoY:")
+auto_python = AutoY("Toyota", 0, 50, "Rojo")
+print(auto_python.color)
+auto_python.estado()
+auto_python.color = "Azul"
+auto_python.estado()
